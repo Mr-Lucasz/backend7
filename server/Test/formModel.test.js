52afadd65657f389a2a0f3537944f4a6c16f8ed2
Lucas.Rodrigues <l.cunha14.lc@gmail.com>
2024-06-18 23:52:20 -0300

    chore: Refactor form submission handling and update dependencies

---
+++ server/Test/formModel.test.js
@@ -0,0 +1,54 @@
+import { defineFeature, loadFeature } from 'jest-cucumber';
+import { insertFormData } from '../formModel';
+
+const feature = loadFeature('./server/Test/validacaoTelefone.feature');
+
+defineFeature(feature, test => {
+  test('Telefone inválido', ({ given, when, then }) => {
+    let formData;
+    let result;
+
+    given('o usuário preencheu os campos do formulário', () => {
+      formData = {
+        nome: 'João',
+        email: 'joao@example.com',
+        telefone: '12345',
+        empresa: 'Empresa XYZ',
+        mensagem: 'Descrição da necessidade',
+        checkbox: 'TRUE'
+      };
+    });
+
+    when('o usuário submete o formulário com o campo "Telefone" em formato inválido', () => {
+      result = insertFormData(formData);
+    });
+
+    then('o sistema deve exibir uma mensagem de erro indicando que o formato do telefone é inválido', () => {
+      expect(result).toEqual({ success: false, message: 'Formato do telefone inválido.' });
+    });
+  });
+
+  test('Telefone válido', ({ given, when, then }) => {
+    let formData;
+    let result;
+
+    given('o usuário preencheu os campos do formulário', () => {
+      formData = {
+        nome: 'João',
+        email: 'joao@example.com',
+        telefone: '12345',
+        empresa: 'Empresa XYZ',
+        mensagem: 'Descrição da necessidade',
+        checkbox: TRUE
+      };
+    });
+
+    when('o usuário submete o formulário com o campo "Telefone" em formato válido', () => {
+      result = insertFormData(formData);
+    });
+
+    then('nenhuma mensagem de erro deve ser exibida', () => {
+      expect(result).toEqual({ success: true });
+    });
+  });
+});
--- server/formModel.js
+++ server/formModel.js
@@ -1,13 +1,20 @@
 // server/formModel.js
 import { supabase } from './Database.js';
-
 export async function insertFormData(data) {
+  const telefoneRegex = /^[0-9]{9}$/;
+
+  if (!telefoneRegex.test(data.telefone)) {
+    return { success: false, message: 'Formato do telefone inválido.' };
+  }
+
   try {
     const { error } = await supabase
         .from('cliente')
         .insert([data]);
 
     if (error) throw error;
+
+    return { success: true };
   } catch (error) {
     console.error('Error details:', error); // Print out the error details
     throw new Error('Falha ao inserir dados');
