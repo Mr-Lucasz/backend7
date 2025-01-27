1419294a63611822cab201a6642cf0f5a3d5ea04
Lucas Rodrigues <lucas.cunhar@iatec.com>
2025-01-27 16:37:07 -0300

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI3MDg3Mw/spectral.js?branch_slug=main&token=e95dcfd2-a589-4bcb-9f60-996bcefbf39f";
+export default { extends: ruleset };
